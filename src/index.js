fa506fd70c54318f1cb70bd933412f9c795fac05
jason <jasonedlewis@gmail.com>
2019-08-29 00:09:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+window.addEventListener('DOMContentLoaded', () => {
+    // console.log('DOM fully loaded and parsed');
+    const picDiv = document.getElementById('dog-image-container');
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => adder(json));
+
+    const adder = (json) => {
+        json.message.forEach(function(mess){
+            let pic = document.createElement('img');
+            pic.setAttribute('src', `${mess}`);
+            picDiv.append(pic);
+        }
+        )
+    };
+}
+);
